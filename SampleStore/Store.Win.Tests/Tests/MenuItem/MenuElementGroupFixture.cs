b0628c545936a51c983960984b1ded68800a0a72
Erich <erich.ledesma@gmail.com>
2012-09-14 13:12:03 +0200

    Groups and Menu elements

---
+++ SampleStore/Store.Win.Tests/Tests/MenuItem/MenuElementGroupFixture.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Store.Controls;
+using Store.Domain;
+
+namespace Store.Win.Tests.Tests.MenuItem
+{
+    [TestFixture]
+    public class MenuElementGroupFixture
+    {
+        private static readonly string GroupCaption = Random.Sentence();
+        private static readonly string GroupTooltip = Random.Sentence();
+        private static readonly Glyph GroupGlyph = Random.Glyph();
+
+        private static readonly string Item1Caption = Random.Sentence();
+        private static readonly string Item1Tooltip = Random.Sentence();
+        private static readonly Glyph Item1Glyph = Random.Glyph();
+
+        private static readonly string Item2Caption = Random.Sentence();
+        private static readonly string Item2Tooltip = Random.Sentence();
+        private static readonly Glyph Item2Glyph = Random.Glyph();
+
+        private MenuStrip _mainMenu;
+        private MenuElementGroup _menuGroup;
+        private MenuElement[] _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new[] {new MenuElement(Item1Caption, Item1Tooltip, Item1Glyph), 
+                new MenuElement(Item2Caption, Item2Tooltip, Item2Glyph)};
+            _menuGroup = new MenuElementGroup
+                             {
+                                 Caption = GroupCaption,
+                                 Glyph = GroupGlyph,
+                                 Tooltip = GroupTooltip,
+                                 Options = _options
+                             };
+
+            _mainMenu = new MenuStrip();
+        }
+
+        [Test]
+        public void AddTo_adds_one_item_to_menu()
+        {
+            _menuGroup.AddTo(_mainMenu);
+
+            var toolStripItems = MainMenuItems();
+            Assert.That(toolStripItems.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddTo_adds_an_element_matching_menuGroup()
+        {
+            _menuGroup.AddTo(_mainMenu);
+
+            var toolStripItem = MainMenuItems().First();
+
+            Assert.That(toolStripItem.Text, Is.EqualTo(_menuGroup.Caption));
+            Assert.That(toolStripItem.ToolTipText, Is.EqualTo(_menuGroup.Tooltip));
+            Assert.That(toolStripItem.Image, Is.EqualTo(_menuGroup.Glyph.Small));
+        }
+
+        [Test]
+        public void AddTo_adds_all_options()
+        {
+            _menuGroup.AddTo(_mainMenu);
+
+            var subMenu = (ToolStripMenuItem) _mainMenu.Items[0];
+
+            Assert.That(subMenu.DropDownItems.Count, Is.EqualTo(2));
+        }
+
+        private IEnumerable<ToolStripItem> MainMenuItems()
+        {
+            return _mainMenu.Items.Cast<ToolStripItem>();
+        }
+
+        [Test]
+        public void AddTo_adds_matching_options()
+        {
+            _menuGroup.AddTo(_mainMenu);
+
+            var subMenu = (ToolStripMenuItem)_mainMenu.Items[0];
+
+            var toolStripItem1 = subMenu.DropDownItems.Cast<ToolStripItem>().First();
+            var toolStripItem2 = subMenu.DropDownItems.Cast<ToolStripItem>().Last();
+
+            Assert.That(toolStripItem1.Text, Is.EqualTo(Item1Caption));
+            Assert.That(toolStripItem1.ToolTipText, Is.EqualTo(Item1Tooltip));
+            Assert.That(toolStripItem1.Image, Is.EqualTo(Item1Glyph.Small));
+
+            Assert.That(toolStripItem2.Text, Is.EqualTo(Item2Caption));
+            Assert.That(toolStripItem2.ToolTipText, Is.EqualTo(Item2Tooltip));
+            Assert.That(toolStripItem2.Image, Is.EqualTo(Item2Glyph.Small));
+        }
+    }
+}
--- SampleStore/Store.Win/Domain/MenuElementGroup.cs
+++ SampleStore/Store.Win/Domain/MenuElementGroup.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 using ActiveWidgets.Controls;
+using ActiveWidgets.Utils;
 using Store.Controls;
 
 namespace Store.Domain
@@ -17,5 +19,28 @@ namespace Store.Domain
 		public Glyph Glyph { get; set; }
 		
 		public IEnumerable<IMenuElement> Options { get; set; }
+
+        /// <summary>
+        /// Adds currrent element group to specified menu strip
+        /// </summary>
+        /// <param name="mainMenu"></param>
+	    public void AddTo(MenuStrip mainMenu)
+        {
+            var menu = new ToolStripMenuItem(Caption);
+            if (Glyph != null)
+                menu.Image = Glyph.Small;
+            menu.ToolTipText = Tooltip;
+
+            mainMenu.Items.Add(menu);
+
+            Options
+                .OfType<MenuElement>()
+                .ForEach(element => AddToMenu(element, menu));
+        }
+        private static void AddToMenu(MenuElement menuElement, ToolStripDropDownItem menu)
+        {
+            var item = menu.DropDownItems.Add(menuElement.Caption, menuElement.Glyph.Small);
+            item.ToolTipText = menuElement.Tooltip;
+        }
 	}
 }
