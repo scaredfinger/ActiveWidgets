24ed71a4b36350280d6292e8bf8f37232f37768c
Erich <erich.ledesma@gmail.com>
2012-09-15 16:38:31 +0200

    Glyph extensions created
    3 Methods to get safely Small, Medium and Large images from glyphs, not
    checking if the glyphs them selves are null. Which is an operation
    repeated too often.

---
+++ SampleStore/Store.Win.Tests/Tests/Controls/GlyphExtensionsFixture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using Store.Controls;
+
+namespace Store.Win.Tests.Tests.Controls
+{
+	[TestFixture]
+	public class GlyphExtensionsFixture
+	{
+		private static readonly Image Large = new Bitmap(10, 10);
+		private static readonly Image Medium = new Bitmap(10, 10);
+		private static readonly Image Small = new Bitmap(10, 10);
+		
+		private Glyph _glyph ;
+		
+		[SetUp]
+		public void Setup()
+		{
+			_glyph = new Glyph(Large, Medium, Small);
+		}
+		
+		[Test]
+		public void GetLarge_returns_null_for_null_glyph()
+		{
+			Glyph glyph = null ;
+			
+			var image = glyph.GetLarge();
+			
+			Assert.That(image, Is.Null);
+		}
+		
+		[Test]
+		public void GetLarge_returns_image_if_not_null()
+		{
+			var actual = _glyph.GetLarge();
+			
+			Assert.That(actual, Is.SameAs(Large));
+		}
+		
+		[Test]
+		public void GetMedium_returns_null_for_null_glyph()
+		{
+			Glyph glyph = null ;
+			
+			var image = glyph.GetMedium();
+			
+			Assert.That(image, Is.Null);
+		}
+		
+		[Test]
+		public void GetMedium_returns_image_if_not_null()
+		{
+			var actual = _glyph.GetMedium();
+			
+			Assert.That(actual, Is.SameAs(Medium));
+		}
+	}
+}
---
+++ SampleStore/Store.Win/Controls/GlyphExtensions.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Drawing;
+
+namespace Store.Controls
+{
+	public static class GlyphExtensions
+	{
+		/// <summary>
+		/// Gets the large image of specified glyph.
+		/// </summary>
+		/// <param name="glyph">Glyph to get the image from</param>
+		/// <returns>null if glyph is null, the glyph large image otherwise</returns>
+		public static Image GetLarge(this Glyph glyph)
+		{
+			return glyph == null ? null : glyph.Large;
+		}
+		
+		/// <summary>
+		/// Gets the medium image of specified glyph.
+		/// </summary>
+		/// <param name="glyph">Glyph to get the image from</param>
+		/// <returns>null if glyph is null, the glyph medium image otherwise</returns>
+		public static Image GetMedium(this Glyph glyph)
+		{
+			return glyph == null ? null : glyph.Medium;
+		}
+		
+		/// <summary>
+		/// Gets the small image of specified glyph.
+		/// </summary>
+		/// <param name="glyph">Glyph to get the image from</param>
+		/// <returns>null if glyph is null, the glyph small image otherwise</returns>
+		public static Image GetSmall(this Glyph glyph)
+		{
+			return glyph == null ? null : glyph.Small;
+		}
+	}
+}
