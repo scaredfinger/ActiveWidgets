f50f32dfcb0dae6db6f9f6c0c5c29d7de1aa580c
Erich <erich.ledesma@gmail.com>
2012-09-15 14:09:19 +0200

    PFGrid boundaries tests
    Created a bunch of new test

---
+++ PfGrid.Boundaries/PanelsAndButtons.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace PfGrid.Boundaries
+{
+	partial class PanelsAndButtons
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			CG.Controls.NavigationBar.Office2003NavigationBarRenderer office2003NavigationBarRenderer1 = new CG.Controls.NavigationBar.Office2003NavigationBarRenderer();
+			this.exPanel1 = new CG.Controls.NavigationBar.ExPanel();
+			this.exHyperlink3 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink2 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink1 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.exHyperlink4 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink5 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink6 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink7 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink8 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exHyperlink9 = new CG.Controls.Grid.Buttons.ExHyperlink();
+			this.exPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// exPanel1
+			// 
+			this.exPanel1.BackColor = System.Drawing.Color.White;
+			this.exPanel1.Controls.Add(this.exHyperlink3);
+			this.exPanel1.Controls.Add(this.exHyperlink2);
+			this.exPanel1.Controls.Add(this.exHyperlink1);
+			this.exPanel1.HeaderText = null;
+			this.exPanel1.Location = new System.Drawing.Point(40, 33);
+			this.exPanel1.Name = "exPanel1";
+			office2003NavigationBarRenderer1.BorderColor = System.Drawing.Color.DarkBlue;
+			office2003NavigationBarRenderer1.ColorPreset = CG.Controls.Common.ColorPresetType.Blue;
+			office2003NavigationBarRenderer1.HeaderColorFrom = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(131)))), ((int)(((byte)(211)))));
+			office2003NavigationBarRenderer1.HeaderColorTo = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(55)))), ((int)(((byte)(156)))));
+			office2003NavigationBarRenderer1.HeaderTextColor = System.Drawing.Color.White;
+			office2003NavigationBarRenderer1.PanelBackColor = System.Drawing.Color.White;
+			office2003NavigationBarRenderer1.PanelColorFrom = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(223)))), ((int)(((byte)(251)))));
+			office2003NavigationBarRenderer1.PanelColorTo = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(168)))), ((int)(((byte)(225)))));
+			office2003NavigationBarRenderer1.PanelOverColorFrom = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(210)))));
+			office2003NavigationBarRenderer1.PanelOverColorTo = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(200)))), ((int)(((byte)(106)))));
+			office2003NavigationBarRenderer1.PanelSelectedColorFrom = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(223)))), ((int)(((byte)(137)))));
+			office2003NavigationBarRenderer1.PanelSelectedColorTo = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(156)))), ((int)(((byte)(32)))));
+			office2003NavigationBarRenderer1.PanelTextColor = System.Drawing.Color.Black;
+			this.exPanel1.Renderer = office2003NavigationBarRenderer1;
+			this.exPanel1.Size = new System.Drawing.Size(236, 328);
+			this.exPanel1.TabIndex = 0;
+			this.exPanel1.Text = "exPanel1";
+			// 
+			// exHyperlink3
+			// 
+			this.exHyperlink3.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink3.DrawFocusRect = true;
+			this.exHyperlink3.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink3.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink3.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink3.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink3.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink3.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink3.LinkVisited = false;
+			this.exHyperlink3.Location = new System.Drawing.Point(53, 190);
+			this.exHyperlink3.Name = "exHyperlink3";
+			this.exHyperlink3.NavigateToUrl = false;
+			this.exHyperlink3.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink3.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink3.TabIndex = 2;
+			this.exHyperlink3.Text = "exHyperlink3";
+			this.exHyperlink3.Url = "";
+			this.exHyperlink3.WordWrap = true;
+			// 
+			// exHyperlink2
+			// 
+			this.exHyperlink2.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink2.DrawFocusRect = true;
+			this.exHyperlink2.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink2.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink2.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink2.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink2.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink2.LinkVisited = false;
+			this.exHyperlink2.Location = new System.Drawing.Point(61, 95);
+			this.exHyperlink2.Name = "exHyperlink2";
+			this.exHyperlink2.NavigateToUrl = false;
+			this.exHyperlink2.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink2.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink2.TabIndex = 1;
+			this.exHyperlink2.Text = "exHyperlink2";
+			this.exHyperlink2.Url = "";
+			this.exHyperlink2.WordWrap = true;
+			// 
+			// exHyperlink1
+			// 
+			this.exHyperlink1.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink1.DrawFocusRect = true;
+			this.exHyperlink1.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink1.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink1.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink1.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink1.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink1.LinkVisited = false;
+			this.exHyperlink1.Location = new System.Drawing.Point(46, 43);
+			this.exHyperlink1.Name = "exHyperlink1";
+			this.exHyperlink1.NavigateToUrl = false;
+			this.exHyperlink1.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink1.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink1.TabIndex = 0;
+			this.exHyperlink1.Text = "exHyperlink1";
+			this.exHyperlink1.Url = "";
+			this.exHyperlink1.WordWrap = true;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink4);
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink5);
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink6);
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink7);
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink8);
+			this.flowLayoutPanel1.Controls.Add(this.exHyperlink9);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(320, 33);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 328);
+			this.flowLayoutPanel1.TabIndex = 2;
+			// 
+			// exHyperlink4
+			// 
+			this.exHyperlink4.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink4.DrawFocusRect = true;
+			this.exHyperlink4.Image = global::PfGrid.Boundaries.Properties.Resources.accept;
+			this.exHyperlink4.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink4.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink4.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink4.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink4.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink4.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink4.LinkVisited = false;
+			this.exHyperlink4.Location = new System.Drawing.Point(3, 3);
+			this.exHyperlink4.Name = "exHyperlink4";
+			this.exHyperlink4.NavigateToUrl = false;
+			this.exHyperlink4.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink4.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink4.TabIndex = 0;
+			this.exHyperlink4.Text = "exHyperlink4";
+			this.exHyperlink4.Url = "";
+			this.exHyperlink4.WordWrap = true;
+			// 
+			// exHyperlink5
+			// 
+			this.exHyperlink5.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink5.DrawFocusRect = true;
+			this.exHyperlink5.Image = global::PfGrid.Boundaries.Properties.Resources.backward;
+			this.exHyperlink5.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink5.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink5.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink5.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink5.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink5.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink5.LinkVisited = false;
+			this.exHyperlink5.Location = new System.Drawing.Point(3, 32);
+			this.exHyperlink5.Name = "exHyperlink5";
+			this.exHyperlink5.NavigateToUrl = false;
+			this.exHyperlink5.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink5.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink5.TabIndex = 1;
+			this.exHyperlink5.Text = "exHyperlink5";
+			this.exHyperlink5.Url = "";
+			this.exHyperlink5.WordWrap = true;
+			// 
+			// exHyperlink6
+			// 
+			this.exHyperlink6.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink6.DrawFocusRect = true;
+			this.exHyperlink6.Image = global::PfGrid.Boundaries.Properties.Resources.bug;
+			this.exHyperlink6.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink6.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink6.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink6.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink6.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink6.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink6.LinkVisited = false;
+			this.exHyperlink6.Location = new System.Drawing.Point(3, 61);
+			this.exHyperlink6.Name = "exHyperlink6";
+			this.exHyperlink6.NavigateToUrl = false;
+			this.exHyperlink6.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink6.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink6.TabIndex = 2;
+			this.exHyperlink6.Text = "exHyperlink6";
+			this.exHyperlink6.Url = "";
+			this.exHyperlink6.WordWrap = true;
+			// 
+			// exHyperlink7
+			// 
+			this.exHyperlink7.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink7.DrawFocusRect = true;
+			this.exHyperlink7.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink7.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink7.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink7.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink7.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink7.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink7.LinkVisited = false;
+			this.exHyperlink7.Location = new System.Drawing.Point(3, 90);
+			this.exHyperlink7.Name = "exHyperlink7";
+			this.exHyperlink7.NavigateToUrl = false;
+			this.exHyperlink7.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink7.Size = new System.Drawing.Size(91, 23);
+			this.exHyperlink7.TabIndex = 3;
+			this.exHyperlink7.Text = "exHyperlink7";
+			this.exHyperlink7.Url = "";
+			this.exHyperlink7.WordWrap = true;
+			// 
+			// exHyperlink8
+			// 
+			this.exHyperlink8.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink8.DrawFocusRect = true;
+			this.exHyperlink8.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink8.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink8.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink8.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink8.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink8.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink8.LinkVisited = false;
+			this.exHyperlink8.Location = new System.Drawing.Point(3, 119);
+			this.exHyperlink8.Name = "exHyperlink8";
+			this.exHyperlink8.NavigateToUrl = false;
+			this.exHyperlink8.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink8.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink8.TabIndex = 4;
+			this.exHyperlink8.Text = "exHyperlink8";
+			this.exHyperlink8.Url = "";
+			this.exHyperlink8.WordWrap = true;
+			// 
+			// exHyperlink9
+			// 
+			this.exHyperlink9.BackColor = System.Drawing.Color.Transparent;
+			this.exHyperlink9.DrawFocusRect = true;
+			this.exHyperlink9.ImageOffset = new System.Drawing.Point(4, 0);
+			this.exHyperlink9.ImageSize = new System.Drawing.Size(16, 16);
+			this.exHyperlink9.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+			this.exHyperlink9.LinkColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.exHyperlink9.LinkColorVisited = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(102)))), ((int)(((byte)(128)))));
+			this.exHyperlink9.LinkUnderline = CG.Controls.Grid.Buttons.EnLinkUnderline.OnHover;
+			this.exHyperlink9.LinkVisited = false;
+			this.exHyperlink9.Location = new System.Drawing.Point(3, 148);
+			this.exHyperlink9.Name = "exHyperlink9";
+			this.exHyperlink9.NavigateToUrl = false;
+			this.exHyperlink9.PaddingText = new System.Windows.Forms.Padding(4, 1, 4, 1);
+			this.exHyperlink9.Size = new System.Drawing.Size(100, 23);
+			this.exHyperlink9.TabIndex = 5;
+			this.exHyperlink9.Text = "exHyperlink9";
+			this.exHyperlink9.Url = "";
+			this.exHyperlink9.WordWrap = true;
+			// 
+			// PanelsAndButtons
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(634, 407);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.exPanel1);
+			this.Name = "PanelsAndButtons";
+			this.Text = "PanelsAndButtons";
+			this.exPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink9;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink8;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink7;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink6;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink5;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink4;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink1;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink2;
+		private CG.Controls.Grid.Buttons.ExHyperlink exHyperlink3;
+		private CG.Controls.NavigationBar.ExPanel exPanel1;
+	}
+}
---
+++ PfGrid.Boundaries/PanelsAndButtons.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PfGrid.Boundaries
+{
+	/// <summary>
+	/// Description of PanelsAndButtons.
+	/// </summary>
+	public partial class PanelsAndButtons : Form
+	{
+		public PanelsAndButtons()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
