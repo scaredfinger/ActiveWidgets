b69abfb9d23b02f5be731bb63419026c45c4a2ee
Erich <erich.ledesma@gmail.com>
2012-09-16 20:01:22 +0200

    Type constraints extensions created
    Some methods to filter type collections

---
+++ ActiveWidgets.Tests/Units/Utils/TypeConstraintsExtensionsFixture.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using ActiveWidgets.Utils;
+using NUnit.Framework;
+
+/// <summary>
+/// This class is needed as a testcase
+/// </summary>
+public class RootLevel
+{
+}
+
+namespace ActiveWidgets.Tests.Units.Utils
+{
+    [TestFixture]
+    public class TypeConstraintsExtensionsFixture
+    {
+        public class DecoratorAttribute : Attribute
+        {
+        }
+
+        [Decorator]
+        public class Decorated
+        {
+        }
+
+        public class NotDecorated
+        {
+        }
+
+        private interface II
+        {
+        }
+
+        private interface IA
+        {
+        }
+
+        private class A : IA
+        {
+        }
+
+        private abstract class B
+        {
+        }
+
+        [Test]
+        public void Implements_returns_true_for_implemented_interfaces()
+        {
+            var result = typeof(A).Implements(typeof(IA));
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Implements_returns_true_for_implemented_interfaces_generic()
+        {
+            var result = typeof(A).Implements<IA>();
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Implements_returns_false_for_implemented_interfaces()
+        {
+            var result = typeof(A).Implements(typeof(II));
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Implements_returns_false_for_implemented_interfaces_generic()
+        {
+            var result = typeof(A).Implements<II>();
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsAssignableTo_returns_true_for_assignable()
+        {
+            var result = typeof(A).IsAssignableTo(typeof(object));
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsAssignableTo_returns_false_for_non_assignable()
+        {
+            var result = typeof(A).IsAssignableTo(typeof(int));
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsAssignableTo_returns_true_for_assignable_generic()
+        {
+            var result = typeof(A).IsAssignableTo<object>();
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsAssignableTo_returns_false_for_non_assignable_generic()
+        {
+            var result = typeof(A).IsAssignableTo<int>();
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsInNamespace_returns_true_for_matching()
+        {
+            var result = typeof(A).IsInNamespace("ActiveWidgets.Tests.Units.Utils");
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsInNamespace_returns_false_for_non_matching()
+        {
+            var result = typeof(A).IsInNamespace("Superman");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsInNamespace_retruns_false_for_null()
+        {
+            var result = typeof(RootLevel).IsInNamespace("");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsConcrete_returns_true_for_concrete_types()
+        {
+            var result = typeof(A).IsConcrete();
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsConcrete_return_false_for_abstract_types()
+        {
+            var result = typeof(B).IsConcrete();
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsConcrete_returns_false_for_interfaces()
+        {
+            var result = typeof(IA).IsConcrete();
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsDecoratedWith_with_returns_false_if_not_decorated_with_attribute()
+        {
+            var result = typeof(NotDecorated).IsDecoratedWith(typeof(DecoratorAttribute));
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsDecoratedWith_with_returns_true_if_decorated_with_attribute()
+        {
+            var result = typeof(Decorated).IsDecoratedWith(typeof(DecoratorAttribute));
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsDecoratedWith_generic_with_returns_false_if_not_decorated_with_attribute()
+        {
+            var result = typeof(NotDecorated).IsDecoratedWith<DecoratorAttribute>();
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsDecoratedWith_generic_with_returns_true_if_decorated_with_attribute()
+        {
+            var result = typeof(Decorated).IsDecoratedWith<DecoratorAttribute>();
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsNotDecoratedWith_returns_true_if_not_decorated_with_attribute()
+        {
+            var result = typeof(NotDecorated).IsNotDecoratedWith(typeof(DecoratorAttribute));
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsNotDecoratedWith_returns_false_if_decorated_with_attribute()
+        {
+            var result = typeof(Decorated).IsNotDecoratedWith(typeof(DecoratorAttribute));
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsNotDecoratedWith_generic_with_returns_true_if_not_decorated_with_attribute()
+        {
+            var result = typeof(NotDecorated).IsNotDecoratedWith<DecoratorAttribute>();
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsNotDecoratedWith_generic_with_returns_false_if_decorated_with_attribute()
+        {
+            var result = typeof(Decorated).IsNotDecoratedWith<DecoratorAttribute>();
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ ActiveWidgets/Utils/TypeConstraintsExtensions.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+
+namespace ActiveWidgets.Utils
+{
+    /// <summary>
+    /// Represents filters that can be used when registered a bunch of types into a container
+    /// </summary>
+    /// <seealso cref="RegistrationExtensions"/>
+    public static class TypeConstraintsExtensions
+    {
+        /// <summary>
+        /// Returns true when a type implements a certain interface
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="interface"></param>
+        /// <returns></returns>
+        public static bool Implements(this Type type, Type @interface)
+        {
+            return type.GetInterfaces().Any(x => x == @interface);
+        }
+        
+        /// <summary>
+        /// Returns true when a type implements a certain interface
+        /// </summary>
+        /// <param name="type"></param>
+        /// <typeparamref name="TType"/>
+        /// <returns></returns>
+        public static bool Implements<TType>(this Type type)
+        {
+            return type.Implements(typeof (TType));
+        }
+
+        /// <summary>
+        /// Returns true when a type is assignable to some other type. It's is just a revert 
+        /// logic for <see cref="Type.IsAssignableFrom(Type)"/>
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="base"></param>
+        /// <returns></returns>
+        public static bool IsAssignableTo(this Type type, Type @base)
+        {
+            return @base.IsAssignableFrom(type);
+        }
+
+        /// <summary>
+        /// Returns true when a type is assignable to some other type. It's is just a revert 
+        /// logic for <see cref="Type.IsAssignableFrom(Type)"/>
+        /// </summary>
+        /// <param name="type"></param>
+        /// <typeparamref name="TType"/>
+        /// <returns></returns>
+        public static bool IsAssignableTo<TType>(this Type type)
+        {
+            return type.IsAssignableTo(typeof (TType));
+        }
+
+        /// <summary>
+        /// Returns true then a type is in specified namespace
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="namespaceFragment"></param>
+        /// <returns></returns>
+        public static bool IsInNamespace(this Type type, string namespaceFragment)
+        {
+            return type.Namespace != null &&
+                type.Namespace.Contains(namespaceFragment);
+        }
+
+        /// <summary>
+        /// Returns true for non abstract types
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsConcrete(this Type type)
+        {
+            return !type.IsAbstract;
+        }
+
+        /// <summary>
+        /// Gets whether specified type is decorated with specified attribute
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="attribute">Attribute</param>
+        /// <returns>True if type is decorated with attribute, false otherwise</returns>
+        /// <seealso cref="IsDecoratedWith{TAttribute}"/>
+        /// <seealso cref="IsNotDecoratedWith"/>
+        public static bool IsDecoratedWith(this Type type, Type attribute)
+        {
+            return type.GetCustomAttributes(attribute, true).Any();
+        }
+
+        /// <summary>
+        /// Gets whether specified type is decorated with specified attribute
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <typeparam name="TAttribute">Attribute</typeparam>
+        /// <returns>True if type is decorated with attribute, false otherwise</returns>
+        /// <seealso cref="IsDecoratedWith"/>
+        /// <seealso cref="IsNotDecoratedWith{TAttribute}"/>
+        public static bool IsDecoratedWith<TAttribute>(this Type type)
+            where TAttribute : Attribute
+        {
+            return type.IsDecoratedWith(typeof (TAttribute));
+        }
+
+        /// <summary>
+        /// Gets whether specified type is not decorated with specified attribute
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="attribute">Attribute</param>
+        /// <returns>True if type is not decorated with attribute, false otherwise</returns>
+        /// <seealso cref="IsNotDecoratedWith{TAttribute}"/>
+        /// <seealso cref="IsDecoratedWith"/>
+        public static bool IsNotDecoratedWith(this Type type, Type attribute)
+        {
+            return !type.IsDecoratedWith(attribute);
+        }
+
+        /// <summary>
+        /// Gets whether specified type is not decorated with specified attribute
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <typeparam name="TAttribute">Attribute</typeparam>
+        /// <returns>True if type is not decorated with attribute, false otherwise</returns>
+        /// <seealso cref="IsNotDecoratedWith"/>
+        /// <seealso cref="IsDecoratedWith{TAttribute}"/>
+        public static bool IsNotDecoratedWith<TAttribute>(this Type type)
+            where TAttribute : Attribute
+        {
+            return ! type.IsDecoratedWith(typeof(TAttribute));
+        }
+    }
+}
